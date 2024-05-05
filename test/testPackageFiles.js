f14d82ff8ef58541a2137675ae0c3cca1f5b3dd7
Ben <bmfk_m@yahoo.de>
2024-05-05 13:12:10 +0200

    Update testPackageFiles.js
    new version of legacy testing

--- test/testPackageFiles.js
+++ test/testPackageFiles.js
@@ -7,14 +7,18 @@
 const expect = require('chai').expect;
 const fs     = require('fs');
 
+// node_modules/@iobroker/legacy-testing/tests/testPackageFiles.js
+const adapterDir = process.env.IOBROKER_ROOT_DIR || `${__dirname}/../../../../`;
+const alternativeAdapterDir = `${__dirname}/../../../../`;
+
 describe('Test package.json and io-package.json', () => {
     it('Test package files', done => {
         console.log();
 
-        const fileContentIOPackage = fs.readFileSync(__dirname + '/../io-package.json', 'utf8');
+        const fileContentIOPackage = fs.readFileSync(`${adapterDir}/io-package.json`, 'utf8');
         const ioPackage = JSON.parse(fileContentIOPackage);
 
-        const fileContentNPMPackage = fs.readFileSync(__dirname + '/../package.json', 'utf8');
+        const fileContentNPMPackage = fs.readFileSync(`${adapterDir}/package.json`, 'utf8');
         const npmPackage = JSON.parse(fileContentNPMPackage);
 
         expect(ioPackage).to.be.an('object');
@@ -33,60 +37,70 @@ describe('Test package.json and io-package.json', () => {
         expect(npmPackage.author, 'ERROR: Author in package.json needs to exist').to.exist;
         expect(ioPackage.common.authors, 'ERROR: Authors in io-package.json needs to exist').to.exist;
 
-        expect(ioPackage.common.license, 'ERROR: License missing in io-package in common.license').to.exist;
+        expect(ioPackage.common.license || ioPackage.common.licenseInformation?.license, 'ERROR: License missing in io-package in common.license or in common.licenseInformation.license').to.exist;
 
-        if (ioPackage.common.name.indexOf('template') !== 0) {
+        if (ioPackage.common.name.includes('template')) {
             if (Array.isArray(ioPackage.common.authors)) {
                 expect(ioPackage.common.authors.length, 'ERROR: Author in io-package.json needs to be set').to.not.be.equal(0);
                 if (ioPackage.common.authors.length === 1) {
                     expect(ioPackage.common.authors[0], 'ERROR: Author in io-package.json needs to be a real name').to.not.be.equal('my Name <my@email.com>');
                 }
-            }
-            else {
+            } else {
                 expect(ioPackage.common.authors, 'ERROR: Author in io-package.json needs to be a real name').to.not.be.equal('my Name <my@email.com>');
             }
-        }
-        else {
+        } else {
             console.log('WARNING: Testing for set authors field in io-package skipped because template adapter');
             console.log();
         }
-        expect(fs.existsSync(__dirname + '/../README.md'), 'ERROR: README.md needs to exist! Please create one with description, detail information and changelog. English is mandatory.').to.be.true;
+
+        expect(fs.existsSync(`${adapterDir}/README.md`) || fs.existsSync(`${alternativeAdapterDir}/README.md`), 'ERROR: README.md needs to exist! Please create one with description, detail information and changelog. English is mandatory.').to.be.true;
+
         if (!ioPackage.common.titleLang || typeof ioPackage.common.titleLang !== 'object') {
             console.log('WARNING: titleLang is not existing in io-package.json. Please add');
             console.log();
         }
-        if (
-            ioPackage.common.title.indexOf('iobroker') !== -1 ||
-            ioPackage.common.title.indexOf('ioBroker') !== -1 ||
-            ioPackage.common.title.indexOf('adapter') !== -1 ||
-            ioPackage.common.title.indexOf('Adapter') !== -1
+
+        if (ioPackage.common.title &&
+            (ioPackage.common.title.includes('iobroker') ||
+            ioPackage.common.title.includes('ioBroker') ||
+            ioPackage.common.title.includes('adapter') ||
+            ioPackage.common.title.includes('Adapter'))
         ) {
             console.log('WARNING: title contains Adapter or ioBroker. It is clear anyway, that it is adapter for ioBroker.');
             console.log();
         }
 
         if (!ioPackage.common.controller && !ioPackage.common.onlyWWW && !ioPackage.common.noConfig) {
-            if (!ioPackage.common.materialize || !fs.existsSync(__dirname + '/../admin/index_m.html')) {
-                console.log('WARNING: Admin3 support is missing! Please add it');
-                console.log();
+            if (ioPackage.common.materialize || (ioPackage.common.adminUI && ioPackage.common.adminUI.conifg === 'materialize')) {
+                expect(fs.existsSync(`${adapterDir}/admin/index_m.html`), 'Admin3 support is enabled in io-package.json, but index_m.html is missing!').to.be.true;
             }
-            if (ioPackage.common.materialize) {
-                expect(fs.existsSync(__dirname + '/../admin/index_m.html'), 'Admin3 support is enabled in io-package.json, but index_m.html is missing!').to.be.true;
+            if (ioPackage.common.jsonConfig || (ioPackage.common.adminUI && ioPackage.common.adminUI.conifg === 'json')) {
+                expect(fs.existsSync(`${adapterDir}/admin/jsonConfig.json`) || fs.existsSync(`${adapterDir}/admin/jsonConfig.json5`), 'Admin3 support is enabled in io-package.json, but jsonConfig.json(5) is missing!').to.be.true;
+            }
+            if (ioPackage.common.adminUI && ioPackage.common.adminUI.custom === 'json') {
+                expect(fs.existsSync(`${adapterDir}/admin/jsonCustom.json`) || fs.existsSync(`${adapterDir}/admin/jsonCustom.json5`), 'Custom config support is enabled in io-package.json, but jsonCustom.json(5) is missing!').to.be.true;
+            }
+            if (ioPackage.common.adminUI && ioPackage.common.adminUI.tab === 'html') {
+                expect(fs.existsSync(`${adapterDir}/admin/tab.html`) || fs.existsSync(`${adapterDir}/admin/tab_m.html`), 'HTML-Tab support is enabled in io-package.json, but tab(_m).html is missing!').to.be.true;
             }
         }
 
-        const licenseFileExists = fs.existsSync(__dirname + '/../LICENSE');
-        const fileContentReadme = fs.readFileSync(__dirname + '/../README.md', 'utf8');
-        if (fileContentReadme.indexOf('## Changelog') === -1) {
+        const licenseFileExists = fs.existsSync(`${adapterDir}/LICENSE`);
+        const fileContentReadme = fs.existsSync(`${alternativeAdapterDir}/README.md`) ? fs.readFileSync(`${alternativeAdapterDir}/README.md`, 'utf8') : fs.readFileSync(`${adapterDir}/README.md`, 'utf8');
+
+        if (!fileContentReadme.includes('## Changelog')) {
             console.log('Warning: The README.md should have a section ## Changelog');
             console.log();
         }
-        expect((licenseFileExists || fileContentReadme.indexOf('## License') !== -1), 'A LICENSE must exist as LICENSE file or as part of the README.md').to.be.true;
+
+        expect((licenseFileExists || fileContentReadme.includes('## License')), 'A LICENSE must exist as LICENSE file or as part of the README.md').to.be.true;
+
         if (!licenseFileExists) {
             console.log('Warning: The License should also exist as LICENSE file');
             console.log();
         }
-        if (fileContentReadme.indexOf('## License') === -1) {
+
+        if (!fileContentReadme.includes('## License')) {
             console.log('Warning: The README.md should also have a section ## License to be shown in Admin3');
             console.log();
         }
